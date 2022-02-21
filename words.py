1a47af49a079e2ddeb91baf539c0f7c87f0d36b4
Nicolas Vanz <nicolas.vanz@outlook.com>
2022-02-21 19:46:58 -0300

    modified suggestion algorithm

--- words.py
+++ words.py
@@ -3,11 +3,47 @@ import unidecode
 class Words:
     def __init__(self):
         self.words = get_words()
+        self.all_words = get_words()
+        self.word_length = 5
     
-    def guess(self, correct, present, wrong):
-        self.process_correct(correct)
-        self.process_present(present)
-        self.process_wrong(wrong)
+    def guess(self, guess, info):
+        correct_idx = []
+        present_idx = []
+        wrong_idx   = []
+
+        error = False
+        for i in range(self.word_length):
+            if info[i] == 'g':
+                correct_idx.append((i, guess[i]))
+            elif info[i] == 'y':
+                present_idx.append((i, guess[i]))
+            elif info[i] == 'r':
+                wrong_idx.append(guess[i])
+            else:
+                error = True
+                break
+        
+        wrong_idx = list(set(wrong_idx))
+        for w in wrong_idx:
+            for c in correct_idx:
+                if c[1] == w:
+                    wrong_idx.remove(w)
+            for p in present_idx:
+                if p[1] == w:
+                    wrong_idx.remove(w)
+        
+        if error:
+            print("error. this guess was not processed")
+            return
+
+        if (guess in self.all_words):
+            self.all_words.remove(guess)
+        else:
+            print("this word is not registered!")
+
+        self.process_correct(correct_idx)
+        self.process_present(present_idx)
+        self.process_wrong(wrong_idx)
         print(f"suggested: {self.suggested_guess()}")
 
     def process_correct(self, correct):
@@ -46,33 +82,32 @@ class Words:
                 break
     
     def suggested_guess(self):
-        counter = {}
+        if len(self.words) == 1:
+            return self.words[0]
+    
+        counter = {chr(i) : 0 for i in range(97, 123)}
+        counter['ç'] = 0
+
         total = len(self.words)
 
         # counting ocurrencies
         for word in self.words:
             for letter in set(word):
-                try:
-                    counter[letter] += 1
-                except KeyError:
-                    counter[letter] = 0
+                counter[letter] += 1
 
         # measuring how good the letter is in a guess.
         # we are looking for letters that appear in half of the possible words
         for key, value in counter.items():
-            counter[key] = abs((value/total) - 0.5)
+            counter[key] = abs((value/total) - 0.5) + 0.5
 
         # measuring how good is a word in a guess. We already know which letters
         # are good. We use this here
         values = []
-        for word in self.words.copy():
-            value = 0
+        for word in self.all_words:
+            value = 1
             word_set = set(word)
             for letter in word_set:
-                try:
-                    value += counter[letter]
-                except KeyError:
-                    value += 0.5
+                value *= counter[letter]
             values.append((word, value))
         
         values = sorted(values, key=lambda x: x[1])
